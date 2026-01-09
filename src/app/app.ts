ade94048a51e57269c5cd4920da34e711fd22b51
Adrian Jacek <adrinetor81@gmail.com>
2026-01-09 22:45:08 +0100

    Login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar.component/navbar.component';
-import { FooterComponent } from './components/footer.component/footer.component';
+import {LoginComponent} from './pages/auth/login.component/login.component';
+import {HttpClient} from '@angular/common/http';
+import {AuthService} from './core/services/auth-service/auth.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet,NavbarComponent,LoginComponent],
+  providers: [HttpClient,AuthService],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
