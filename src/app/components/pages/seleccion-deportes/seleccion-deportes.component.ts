2820585d2befaa25805c5e49087c6142c250c9c1
adrinetor86 <adrinetor81@gmail.com>
2026-01-15 12:40:09 +0100

    Pagina v1 Seleccion deporte

--- src/app/components/pages/seleccion-deportes/seleccion-deportes.component.ts
+++ src/app/components/pages/seleccion-deportes/seleccion-deportes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-
+import { CarouselModule } from 'primeng/carousel';
+import {DeportesService} from '../../../core/services/deportes.service';
+import {Deporte} from '../../../models/Deportes';
 @Component({
   selector: 'app-seleccion-deportes',
   imports: [],
@@ -9,14 +11,20 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class SeleccionDeportesComponent implements OnInit {
   public idEvento!: number;
-
-  constructor(private _activeRoute: ActivatedRoute) {}
+  public deportes!: Array<Deporte>;
+  constructor(private _activeRoute: ActivatedRoute,private _deportesService: DeportesService) {}
 
   ngOnInit(): void {
     this._activeRoute.params.subscribe((parametros: Params) => {
       if (parametros['idEvento'] != null) {
+
         this.idEvento = Number(parametros['idEvento']);
         console.log('ID del evento:', this.idEvento);
+
+        this._deportesService.getDeportesEvento(this.idEvento).subscribe(value => {
+
+          this.deportes= value;
+        });
       }
     });
   }
---
+++ src/app/models/Deportes.ts
@@ -0,0 +1,7 @@
+﻿export class Deporte{
+  constructor(
+    public idActividad: number,
+    public nombre: string,
+    public minimoJugadores: number
+  ){}
+}
